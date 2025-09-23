9d6f78866254754638615a08c7cb6c922efb92e1
ShiomiyaRinne <56505148+ShiomiyaRinne@users.noreply.github.com>
2025-09-23 23:10:56 +0800

    AspectRegistry

--- src/main/java/team/torka/thaumicrecords/registry/AspectRegistry.java
+++ src/main/java/team/torka/thaumicrecords/registry/AspectRegistry.java
@@ -3,16 +3,28 @@ package team.torka.thaumicrecords.registry;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NewRegistryEvent;
+import net.neoforged.neoforge.registries.RegistryBuilder;
 import team.torka.thaumicrecords.ThaumicRecords;
 import team.torka.thaumicrecords.api.aspect.Aspect;
 
+@EventBusSubscriber(modid = ThaumicRecords.MOD_ID)
 public class AspectRegistry {
     public static final ResourceKey<Registry<Aspect>> ASPECTS_KEY =
             ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(ThaumicRecords.MOD_ID, "aspects"));
 
     public static final DeferredRegister<Aspect> ASPECTS = DeferredRegister.create(ASPECTS_KEY, ThaumicRecords.MOD_ID);
+
+    public static Registry<Aspect> ASPECT_REGISTRY = null;
+
+    @SubscribeEvent
+    public static void onNewRegistryEvent(NewRegistryEvent event) {
+        ASPECT_REGISTRY = event.create(new RegistryBuilder<>(ASPECTS_KEY));
+    }
 
     /*@formatter:off*/
     public static final DeferredHolder<Aspect,Aspect> AER=ASPECTS.register(Aspect.AER.getName(),()->Aspect.AER);
