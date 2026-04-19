69a9c07fae2ab618e64d5dc72fe59aecf9ecc1ae
ShiomiyaRinne <56505148+ShiomiyaRinne@users.noreply.github.com>
2026-04-20 02:04:39 +0800

    wand render

--- src/main/java/team/torka/thaumicrecords/client/model/WandModel.java
+++ src/main/java/team/torka/thaumicrecords/client/model/WandModel.java
@@ -20,15 +20,15 @@ public class WandModel {
                         .texOffs(0, 8)
                         .addBox(-1.0F, -1.0F, -1.0F, 2, 18, 2),
                 PartPose.offset(0.0F, 2.0F, 0.0F));
-        root.addOrReplaceChild("cap", CubeListBuilder.create()
+        root.addOrReplaceChild("cap", CubeListBuilder.create().mirror()
                         .texOffs(0, 0)
                         .addBox(-1.0F, -1.0F, -1.0F, 2, 2, 2),
                 PartPose.offset(0.0F, 0.0F, 0.0F));
-        root.addOrReplaceChild("cap_bottom", CubeListBuilder.create()
+        root.addOrReplaceChild("cap_bottom", CubeListBuilder.create().mirror()
                         .texOffs(0, 0)
                         .addBox(-1.0F, -1.0F, -1.0F, 2, 2, 2),
                 PartPose.offset(0.0F, 20.0F, 0.0F));
-        LayerDefinition layer = LayerDefinition.create(mesh, 64, 32);
+        LayerDefinition layer = LayerDefinition.create(mesh, 32, 32);
         ModelPart bakedRoot = layer.bakeRoot();
         this.rod = bakedRoot.getChild("rod");
         this.cap = bakedRoot.getChild("cap");
