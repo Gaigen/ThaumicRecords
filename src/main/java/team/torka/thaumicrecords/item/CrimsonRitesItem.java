32518d2ed29b8d21a50a3c2d0cc184bf831ca49b
ShiomiyaRinne <56505148+ShiomiyaRinne@users.noreply.github.com>
2026-06-01 02:08:27 +0800

    血腥仪式物品添加研究

--- src/main/java/team/torka/thaumicrecords/item/CrimsonRitesItem.java
+++ src/main/java/team/torka/thaumicrecords/item/CrimsonRitesItem.java
@@ -7,8 +7,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 import team.torka.thaumicrecords.api.helper.ResearchHelper;
 import team.torka.thaumicrecords.registry.ResearchRegistry;
+
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class CrimsonRitesItem extends Item {
 
@@ -16,7 +19,9 @@ public class CrimsonRitesItem extends Item {
         super(new Item.Properties().stacksTo(1));
     }
 
+    @NotNull
     @Override
+    @ParametersAreNonnullByDefault
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
         if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
