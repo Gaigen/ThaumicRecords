bbbeb51632563508419d7eb19c68ac7e5d94e8dc
ShiomiyaRinne <56505148+ShiomiyaRinne@users.noreply.github.com>
2026-06-01 02:08:08 +0800

    血腥仪式物品添加研究

--- src/main/java/team/torka/thaumicrecords/item/CrimsonRitesItem.java
+++ src/main/java/team/torka/thaumicrecords/item/CrimsonRitesItem.java
@@ -1,6 +1,14 @@
 package team.torka.thaumicrecords.item;
 
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import team.torka.thaumicrecords.api.helper.ResearchHelper;
+import team.torka.thaumicrecords.registry.ResearchRegistry;
 
 public class CrimsonRitesItem extends Item {
 
@@ -8,4 +16,14 @@ public class CrimsonRitesItem extends Item {
         super(new Item.Properties().stacksTo(1));
     }
 
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack itemStack = player.getItemInHand(hand);
+        if (!level.isClientSide && player instanceof ServerPlayer serverPlayer) {
+            if (!ResearchHelper.isResearchCompleted(serverPlayer, ResearchRegistry.CRIMSON.getId())) {
+                ResearchHelper.completeResearch(serverPlayer, ResearchRegistry.CRIMSON.getId());
+            }
+        }
+        return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
+    }
 }
